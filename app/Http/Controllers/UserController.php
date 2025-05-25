d939caf0dca46cdc388876333d0fad03c86c469d
pandutria <panduadyatama029@gmail.com>
2025-05-25 22:05:08 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $request->validate([
+                'role' => 'required|string',
+                'fullname' => 'required|string',
+                'username' => 'required|string|unique:users',
+                'password' => 'required|string',
+                'image_url' => 'string'
+            ]);
+
+            $user = new User();
+            $user->role = $request->role;
+            $user->fullname = $request->fullname;
+            $user->username = $request->username;
+            $user->password = Hash::make($request->password);
+            $user->image_url = null;
+            $user->save();
+
+            // $token = $user->createToken('access_token')->plainTextToken;
+            
+            return response()->json([
+                'user' => $user,
+                // 'token' => $token,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function login(Request $request) {
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken('access_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function me() {
+        $user = Auth::user();
+        return response()->json($user);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'role',
+        'fullname',
+        'username',
+        'password',
+        'image_url'
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
